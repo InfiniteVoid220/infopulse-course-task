e2b7a83dfa5bfe4e20f36d587baf88882a977271
Alexandr Melnyk <Alexandr Melnyk>
2017-04-27 12:18:39 +0300

    del onclick in html

--- javascript/script.js
+++ javascript/script.js
@@ -1,43 +1,48 @@
+window.addEventListener('load', loaded);
+document.getElementById('show-or-hide-left').addEventListener('click', show_or_hide_left);
+document.getElementById('show-right').addEventListener('click', show_right);
+var buttons_del = document.getElementById('data').getElementsByTagName('button');
+for(let i = 0; i < buttons_del.length; i++){
+  buttons_del[i].addEventListener('click', function(){
+    delete_row(this);
+  });
+}
+var thead_buttons = document.getElementById('tb-block').getElementsByTagName('th');
+for(var i = 0; i < thead_buttons.length-1; i++){
+  thead_buttons[i].addEventListener('click', function(){
+    alert(this.cellIndex);
+    sort_table(this.cellIndex);
+  });
+}
+document.getElementById('customer-field').addEventListener('click', function(){
+  show_or_hide_options(this);
+});
+document.getElementById('type-field').addEventListener('click', function(){
+  show_or_hide_options(this);
+});
+document.getElementById('type-options').addEventListener('click', function(){
+  set_text(event);
+});
+document.getElementById('customer-options').addEventListener('click', function(){
+  set_text(event);
+});
+document.getElementById('butt-add-row').addEventListener('click', function(){
+  add_row(this);
+});
+
 /*-----------------------------------Выпадающий список--------------------------------*/
 
 /*переменные для текущего списка*/
 var DropdownButton=null;//текущий список
 var DropdownOptions=null;//елементи текущего списка
 
-function show_or_hide_options(th) { //прячет или открывает елементи списка 
-
-    if(DropdownButton!=th && DropdownButton!=null) {//скрыть если нажали на другой список
-        DropdownOptions.style.display="none";
-        DropdownButton.style.borderBottom="2px solid #d77016";
-        DropdownButton.style.background="rgba(215, 112, 22,.15) url(../images/expand-less.png) no-repeat right top";
-    }
-
-    DropdownButton=th;//запомнить текущий список
-    DropdownOptions=th.nextElementSibling;//запомнить блок с елементами текущего списка
-
-    //отображение елементов при нажатии на поле списка
-    if(DropdownOptions.style.getPropertyValue("display")=="none"){
-        DropdownOptions.style.display="block";
-        DropdownButton.style.borderBottom="2px solid white";
-        DropdownButton.style.background="rgba(215, 112, 22,.15) url(../images/expand-more.png) no-repeat right top";
-    }   
-    else{//скрыть если список уже открыт
-        DropdownOptions.style.display="none";
-        DropdownButton.style.borderBottom="2px solid #d77016";
-        DropdownButton.style.background="rgba(215, 112, 22,.15) url(../images/expand-less.png) no-repeat right top";
-    }
-    
-}
-
  function set_text(event) {//записывает в поле списка данные полученые с выбраного елемента
     DropdownButton.innerHTML = event.target.innerHTML;
-    DropdownButton.previousElementSibling.value=event.target.innerHTML;
 }
 
 /*------------------------------------------------------------------------------------*/
 
 /*Закрыть меню или список если нажать в другое место окна*/
-document.getElementById('show-or-hide-left').addEventListener('click', show_or_hide_left);
 
 window.onclick = function(event) {
 
@@ -66,6 +71,7 @@ window.onclick = function(event) {
 }
 
 function show_right(){//отобразить правую панель
+    alert("show_right exec");
     document.getElementById("right-menu").style.transform="translateX(-370px) ";
 }
 
@@ -87,8 +93,8 @@ function show_or_hide_left(){//работа слевой панелью
         }
 }
 
+/*             ISCROLL            */
 var myScroll; 
-
 function loaded () {
     myScroll = new IScroll('#wrapper-scroll', {scrollbars: false, mouseWheel: true, interactiveScrollbars: true });
 }
@@ -97,12 +103,17 @@ document.addEventListener('touchmove', function (e) { e.preventDefault(); }, isP
     passive: false
 } : false);
 
+
+
+
 function delete_row(x){
+    alert("delete_row exec");
     var current_row=x.parentElement.parentElement.rowIndex;
     document.getElementById("data").deleteRow(current_row);
 }
 
 function add_row(x){
+    alert('add row exec');
    /* var c = document.getElementById("right-form").children;
     var txt = "";
     var i;
@@ -136,6 +147,8 @@ function toDate(dateStr) {
 }
 
 function sort_table(n) {
+    alert("function sort_table with parameter: " + n );
+  
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
     table = document.getElementById("data");
     switching = true;
