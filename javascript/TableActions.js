32faa1392fcb7a848a0b4aad853dafdd6f654dcc
Андрей <>
2017-05-04 13:22:12 +0300

    upd(update row creation)

--- javascript/TableActions.js
+++ javascript/TableActions.js
@@ -1,8 +1,15 @@
 function tableActions(){
 
-    this.deleteRow = function (x) {
+    function deleteRow(x) {
         var currentRow=x.parentElement.parentElement.rowIndex;
         document.getElementById("data").deleteRow(currentRow);
+    }
+
+    var buttons_del = document.getElementById('data').getElementsByTagName('button');
+    for(let i = 0; i < buttons_del.length; i++){
+        buttons_del[i].addEventListener('click', function(){
+            deleteRow(this);
+        });
     }
 
     this.create_row = function(){
@@ -51,6 +58,18 @@ function tableActions(){
         return is_all_filled_in;       
     }
 
+    function dateDisplay(date){
+        var resultDate="";
+        if(date.getDate()<10) resultDate+="0"+date.getDate()+".";
+        else resultDate+=date.getDate()+".";
+
+        if(date.getMonth()+1<10) resultDate+="0"+(date.getMonth()+1)+".";
+        else resultDate+=date.getMonth()+1+".";
+
+        resultDate+=date.getFullYear();
+        return resultDate;
+    }
+
     function load_data(){
         let fields = [];
         let date;
@@ -58,13 +77,14 @@ function tableActions(){
         fields[0] = document.getElementById("project-name").value;
         
         date = new Date(document.getElementById("due-date").value);
-        fields[1] = date.getDate() + "." + (date.getMonth()+1) + "." + date.getFullYear();
+        alert(dateDisplay(date));
+        fields[1] = dateDisplay(date);
 
         date = new Date(document.getElementById("created").value);
-        fields[2] = date.getDate() + "." + (date.getMonth()+1) + "." + date.getFullYear();
+        fields[2] = dateDisplay(date);
 
         fields[3] = document.getElementById("members").value;
-        fields[4] = document.getElementsByClassName('type-dropdown')[0].getElementsByClassName('dropdown-field')[0].innerHTML;
+        fields[4] = document.getElementsByClassName('type-dropdown')[0].getElementsByClassName('dropdown-field')[0].innerHTML.toUpperCase();
         fields[5] = get_status(fields[1], fields[2]);
         fields[6] = document.getElementsByClassName('customer-dropdown')[0].getElementsByClassName('dropdown-field')[0].innerHTML;      
         
@@ -93,7 +113,6 @@ function tableActions(){
     }
 
     function add_row(fields) {
-        alert("add_row exec");
 
         var table = document.getElementById("data");
         var row = table.insertRow(0);
@@ -106,6 +125,10 @@ function tableActions(){
             if(i == 8){
                 var cell = row.insertCell(i);
                 var but = document.createElement('button');
+                but.addEventListener('click', function(){
+                    deleteRow(this);
+                });
+
                 cell.appendChild(but);
                 continue;
             }
